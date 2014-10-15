0e0919cafca8b324158a079efc8ac06a01ab4ad3
Gavin M. Bell <gavin@6thcolumn.org>
2014-10-16 00:03:20 +0200

    Added new -l (list) flag and...
    Also put in the sexy self documenting code...
    And added usage

--- base/info.sh
+++ base/info.sh
@@ -2,18 +2,41 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 image="$(basename $SCRIPT_DIR)"
 
+usage() {
+    cat<<EOF
+Usage: $0 [options]
+
+options:
+$(sed -n '/^#OPTIONS START/,/#OPTIONS END/ {s/ *\([^)]\+\))[^#]\+#\(.*\)/\1\t: \2/p}' $0)
+EOF
+}
+
 get_ip() {
      docker inspect --format '{{.NetworkSettings.IPAddress}}' $container_name
 }
 
+list_containers_for() {
+    local image=${1:${image}}
+    echo "------------------"
+    echo "Running Containers"
+    echo "------------------"
+    docker ps | grep ${image} | awk '{print $NF}'
+    echo "------------------"
+}
+
 #defaults
 container_dir=/tmp/$image
-while getopts "c:n:" opt; do
+[[ $# == 0 ]] && { list_containers_for ${image} ; exit $?; }
+#OPTIONS START
+while getopts "c:n:l:h" opt; do
     case "$opt" in
         c) container_dir="$OPTARG";;    #defines the directory for the container files
         n) container_name="$OPTARG";;   #defines the container name
+        h) usage && exit 0;; #this help info
+        l) list_containers_for $OPTARG;; #list names running containers
     esac
 done
+#OPTIONS END
 shift $((OPTIND-1))
 [[ -z "$container_name" && "$container_dir" ]] && container_name="$(cat "$container_dir/var/run/container.name")"
 
@@ -21,6 +44,7 @@ shift $((OPTIND-1))
 while (($#)); do
  case "$1" in
     ip) get_ip;;
+     *) usage && exit 1;;
  esac
 
  shift
