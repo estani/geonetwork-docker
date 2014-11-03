b5657db8c2ad494f67f48d11e18cbcdb14d619db
estani <you@example.com>
2014-11-03 22:05:08 +0000

    added nc2es command and crawling

---
+++ base/helper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+image="$(basename $SCRIPT_DIR)"
+
+usage() {
+    cat<<EOF
+Usage: $0 [options]
+
+$(sed -n '1,/^[^#]/ {s/^# *description[: ]*//pI}' $0)
+options:
+$(sed -n '/^#OPTIONS START/,/#OPTIONS END/ {s/ *\([^)]\+\))[^#]\+#\(.*\)/\1\t: \2/p}' $0)
+EOF
+}
+
+fullpath() {
+    local path="$1"
+    if [[ -d "$path" ]]; then
+        echo "$(cd "$path"; pwd)"
+    else
+        echo "$(cd $(dirname "$path"); pwd)/$(basename "$path")"
+    fi
+}
--- publisher/scripts/to_json.py
+++ publisher/scripts/to_json.py
@@ -18,7 +18,7 @@ class SimpleDirectoryParser(object):
     def __init__(self, parsingDescription):
         position = 0
         self.metadict = {}
-        for value in parsingDescription.split(SimpleDirectoryParse.DIR_SEP):
+        for value in parsingDescription.split(SimpleDirectoryParser.DIR_SEP):
             if value != SimpleDirectoryParser.SKIP:
                 self.metadict[position] = value
             position += 1
@@ -42,10 +42,10 @@ class NetCDFFileHandler(object):
         
         realpath = os.path.abspath(filename)
         if realpath.startswith(NetCDFFileHandler.CONTAINER_DATA_DIR):
-            realpath = os.path.join(realpath[len(NetCDFFileHandler.CONTAINER_DATA_DIR):],
-                                        os.getenv(NetCDFFileHandler.HOST_DATA_DIR_VAR).strip('/'))
+            realpath = os.path.join(os.getenv(NetCDFFileHandler.HOST_DATA_DIR_VAR),
+                                    realpath[len(NetCDFFileHandler.CONTAINER_DATA_DIR):])
         if self.path_parser is not None:
-            meta = self.path_parser(realpath)
+            meta = self.path_parser.extract(realpath)
         else:
             meta = {}
         meta['original_path'] = realpath
@@ -68,6 +68,23 @@ class NetCDFFileHandler(object):
         meta['unlimited'] = dim.isunlimited()
         return meta
 
+    def crawl_dir(self, path, exclude=[], include=None):
+        for root, subdirs, files in os.walk(path):
+            for f in files:
+                skip = False
+                current = os.path.join(root, f)
+                for e in exclude:
+                    if e.match(current):
+                        skip = True
+                        break
+                if include is not None and not skip:
+                    for i in include:
+                        if not i.match(current):
+                            skip = True
+                            break
+                if not skip:
+                    yield self.get_metadata(current)
+
     def get_metadata(self, filename):
         meta = self.__extract_from_filename(filename)
         with Dataset(filename, 'r') as f:
@@ -81,6 +98,12 @@ class NetCDFFileHandler(object):
             for dim in f.dimensions:
                 meta['dimensions'][dim] = self.__extract_dimension(f.dimensions[dim])
         return meta
+    
+def process(meta, elasticsearch, show=True, dry_run=True):
+    if show:
+        print json.dumps(meta, indent=2)
+    if not dry_run:
+        es.publish(meta)
 
 def main(args=sys.argv[1:]):
     import argparse
@@ -88,18 +111,32 @@ def main(args=sys.argv[1:]):
     parser.add_argument('files', nargs='+')
     parser.add_argument('--show', action='store_true', help='show produced json')
     parser.add_argument('--dry-run', action='store_true', help="Don't publish anything")
+    parser.add_argument('--dir-structure', help='Metadata directory structure (e.g. /*/institute/model/realm so /a/b/c/d/e -> institute=b, model=c,realm=d) ')
+    parser.add_argument('--exclude-crawl', help='Exclude the given regular expression')
     pargs = parser.parse_args(args)
 
     #handle input properly
-    handler = NetCDFFileHandler()
+    if pargs.dir_structure is not None:
+        path_parser = SimpleDirectoryParser(pargs.dir_structure)
+    else:
+        path_parser = None
+    handler = NetCDFFileHandler(path_parser=path_parser)
+
+    exclude = []
+    if pargs.exclude_crawl:
+        import re
+        exclude.append(re.compile(pargs.exclude_crawl))
+
     es = es_api.ES()
     for filename in pargs.files:
-        file_meta = handler.get_metadata(filename)
-        if pargs.show:
-            print json.dumps(file_meta, indent=2)
-        if not pargs.dry_run:
-            es.publish(file_meta)
-
+        if os.path.isdir(filename):
+            for file_meta in handler.crawl_dir(filename, exclude=exclude):
+                process(file_meta, es, show=pargs.show, dry_run=pargs.dry_run)
+        elif os.path.isfile(filename):
+            file_meta = handler.get_metadata(filename)
+            process(file_meta, es, show=pargs.show, dry_run=pargs.dry_run)
+        else:
+            print "%s is not a file/dir. skipping" % filename
 if __name__ == '__main__':
   main()
 
