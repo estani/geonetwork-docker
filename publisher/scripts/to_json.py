e55c30246ff2886b869df94db2568e137d49dee9
estani <you@example.com>
2014-12-08 16:33:53 +0000

    allow to add any global attribute from the command line

--- publisher/scripts/to_json.py
+++ publisher/scripts/to_json.py
@@ -12,14 +12,33 @@ class SetEncoder(json.JSONEncoder):
         except:
             return str(obj)
 
-def process(meta, elasticsearch, show=True):
+def process(meta, elasticsearch, global_att, show=True):
+    meta['global'].update(global_att)
     meta_json = json.dumps(meta, indent=2, cls=SetEncoder) 
     if show:
         print meta_json
     if elasticsearch:
         elasticsearch.publish(json.loads(meta_json))
 
-def main(args=sys.argv[1:]):
+def main(orig_args=sys.argv[1:]):
+    
+    #work around for handling attributes
+    args = []
+    attr_follows = False
+    global_att = {}
+    for arg in orig_args:
+        if attr_follows:
+            items = arg.split('=',1)
+            if len(items) == 1:
+                global_att[items[0]] = 'true'
+            else:
+                global_att[items[0]] = items[1]
+            attr_follows = False
+        elif arg == '--global':
+            attr_follows = True
+        else:
+            args.append(arg)
+
     import argparse
     parser = argparse.ArgumentParser(description='Extracts metadata from Netcdf files')
     parser.add_argument('files', metavar="FILE/DIR", nargs=1)
@@ -30,6 +49,7 @@ def main(args=sys.argv[1:]):
     #parser.add_argument('--dump', help='Directory where json will get dumped')
     #This is not used here, but used by the calling script. Still we want to show a single help.
     parser.add_argument('--host', help='Elastic search host')
+    parser.add_argument('--global', help='Adds some gobal attribute using "=" as separator (e.g. --global institute=AWI). Can be used multiple times.')
     parser.add_argument('--dir-structure', help='Metadata directory structure (e.g. /*/institute/model/realm so /a/b/c/d/e -> institute=b, model=c,realm=d) ')
     parser.add_argument('--file-structure', help='Metadata File structure. (e.g. institute_model_realm so ABC_mod1_atmos_blah.nc -> institute=ABC, model=mod1,realm=atmos) ')
     parser.add_argument('--file-structure-sep', help='Separator used in the filename for structuring data (default "_")', default='_')
@@ -66,10 +86,10 @@ def main(args=sys.argv[1:]):
     for filename in pargs.files:
         if os.path.isdir(filename):
             for file_meta in handler.crawl_dir(filename, exclude=exclude, include=include):
-                process(file_meta, es, show=pargs.show)
+                process(file_meta, es, global_att, show=pargs.show)
         elif os.path.isfile(filename):
             file_meta = handler.get_metadata(filename)
-            process(file_meta, es, show=pargs.show)
+            process(file_meta, es, global_att, show=pargs.show)
         else:
             print "%s is not a file/dir. skipping" % filename
 if __name__ == '__main__':
